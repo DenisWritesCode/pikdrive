66cff7273651d38866152bfba473e1283932d0e7
DenisWritesCode <deniswritescode@gmail.com>
2021-04-20 03:41:33 +0200

    Getting API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const baseUrl = "https://jsonplaceholder.typicode.com/users"; //"https://cors.bridged.cc/https://codechallenge.pikdrive.com/api/orders/"; // pesky CORS error.
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    Axios.get(baseUrl).then((res) => {
+      //const data = res.data.data; //Using 2 cause they return nested objects
+      const data = res.data;
+      console.log(data);
+      setProducts(res.data);
+    });
+  }, []);
+
   return (
     <div className="App">
+      {products.map((item) => {
+        return <p key={item.id}>{item.name}</p>;
+      })}
+      <button>Fetch Products</button>
       <h1>React working!!!</h1>
     </div>
   );
