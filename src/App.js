247e100ac2f5f7c42c39a2537a8d2f308c6ab37e
DenisWritesCode <deniswritescode@gmail.com>
2021-04-23 19:23:52 +0200

    Removed BrowserRouter

--- src/App.js
+++ src/App.js
@@ -17,54 +17,52 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
-      <div className="App">
-        <nav>
-          <h1>PikDrive</h1>
-          <ul>
-            <li>
-              <Link to="/">Orders</Link>
-            </li>
-            <li>
-              <Link to="/products">Products</Link>
-            </li>
-            <li>
-              <Link to="/suppliers">Suppliers</Link>
-            </li>
-            <li>
-              <Link to="/cart">
-                <i className="fas fa-shopping-cart"></i>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="container">
-          <Switch>
-            <Route exact path="/">
-              <Orders displayDate={displayDate} />
-            </Route>
-            <Route path="/products">
-              <Products displayDate={displayDate} cart={cart} />
-            </Route>
-            <Route path="/suppliers">
-              <Suppliers displayDate={displayDate} />
-            </Route>
-            <Route path="/top-sales">
-              <TopSales displayDate={displayDate} />
-            </Route>
-            <Route path="/cart">
-              <Cart cart={cart} setCart={setCart} />
-            </Route>
-          </Switch>
-        </div>
-        <footer>
-          <p className="copyright">&copy; 2021</p>
-          <p className="link">
-            Designed By <a href="denismutinda.com">DenisWritesCode</a>
-          </p>
-        </footer>
+    <div className="App">
+      <nav>
+        <h1>PikDrive</h1>
+        <ul>
+          <li>
+            <Link to="/">Orders</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            <Link to="/suppliers">Suppliers</Link>
+          </li>
+          <li>
+            <Link to="/cart">
+              <i className="fas fa-shopping-cart"></i>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="container">
+        <Switch>
+          <Route exact path="/">
+            <Orders displayDate={displayDate} />
+          </Route>
+          <Route path="/products">
+            <Products displayDate={displayDate} cart={cart} />
+          </Route>
+          <Route path="/suppliers">
+            <Suppliers displayDate={displayDate} />
+          </Route>
+          <Route path="/top-sales">
+            <TopSales displayDate={displayDate} />
+          </Route>
+          <Route path="/cart">
+            <Cart cart={cart} setCart={setCart} />
+          </Route>
+        </Switch>
       </div>
-    </BrowserRouter>
+      <footer>
+        <p className="copyright">&copy; 2021</p>
+        <p className="link">
+          Designed By <a href="denismutinda.com">DenisWritesCode</a>
+        </p>
+      </footer>
+    </div>
   );
 }
 
